e8921c0301daffdad1dac5761f2df33706c2ffdc
amymmcd <amymargaret22@gmail.com>
2025-03-18 22:14:13 -0600

    Bootstrap updates

--- frontend/src/ProjectList.tsx
+++ frontend/src/ProjectList.tsx
@@ -21,15 +21,17 @@ function ProjectList() {
       <h1>Water Projects</h1>
       <br />
       {projects.map((p) => (
-        <div id="projectCard">
-          <h3>{p.projectName}</h3>
-          <ul>
-            <li>Project Type: {p.projectType}</li>
-            <li>Regional Program: {p.projectRegionalProgram}</li>
-            <li>Impact: {p.projectImpact}</li>
-            <li>Phase: {p.projectPhase}</li>
-            <li>Status: {p.projectFunctionalityStatus}</li>
-          </ul>
+        <div id="projectCard" className="card" key={p.projectId}>
+          <h3 className="card-title">{p.projectName}</h3>
+          <div className="card-body">
+            <ul className="list-unstyled">
+              <li><strong>Project Type: </strong>{p.projectType}</li>
+              <li><strong>Regional Program: </strong>{p.projectRegionalProgram}</li>
+              <li><strong>Impact: </strong>{p.projectImpact}</li>
+              <li><strong>Phase: </strong>{p.projectPhase}</li>
+              <li><strong>Status: </strong>{p.projectFunctionalityStatus}</li>
+            </ul>
+          </div>
         </div>
       ))}
     </>
