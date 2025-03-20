e3fcb72a04f83c499f72bd77f370d16bb33e0122
amymmcd <amymargaret22@gmail.com>
2025-03-19 22:02:55 -0600

    updates

--- backend/WaterProject.API/Controllers/WaterController.cs
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -13,10 +13,22 @@ public class WaterController : ControllerBase
     public WaterController(WaterDbContext temp) => _context = temp; //using lambda function instead of normal way
 
     [HttpGet("AllProjects")]
-    public IEnumerable<Project> GetAllProjects()
+    public IActionResult GetAllProjects(int pageSize = 5, int pageNum = 1) //because you are returning an object instead of a list of projects, use IActionResult instead of IEnumerable
     {
-        var result = _context.Projects.ToList();
-        return result;
+        var result = _context.Projects
+            .Skip((pageNum - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+        
+        var projectCount = _context.Projects.Count();
+
+        var someObject = new //or you could create a class and then create an instance of that class. or you could build the object directly in the return statement (not ideal). 
+        {
+            Projects = result,
+            ProjectCount = projectCount
+        };
+
+        return Ok(someObject); //okay returns http 200 and converts it to json
     }
 
     [HttpGet("FunctionalProjects")]
